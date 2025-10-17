7944dc96e82f41bf606866dd21f4a42ef16863bb
Hugo Machado <hugo_machado02@hotmail.com>
2025-10-17 02:09:06 -0300

    :sparkles: feat: Commit inicial

---
+++ src/main/java/com/hugomachadodev/CalculaIdadeTsi2025/CalculaIdadeTsi2025Application.java
@@ -0,0 +1,13 @@
+package com.hugomachadodev.CalculaIdadeTsi2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaIdadeTsi2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaIdadeTsi2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugomachadodev/CalculaIdadeTsi2025/CalculaIdadeTsi2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugomachadodev.CalculaIdadeTsi2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculaIdadeTsi2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
